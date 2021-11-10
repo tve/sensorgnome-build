6808afe6ad800153fec7bcc9cf2c6bebd5f4a3c3
Thorsten von Eicken <tve@voneicken.com>
2021-11-10 12:38:49 -0800

    Add script to init sd-card

---
+++ init-sdcard.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -e
+if [[ ! -d /run/media/$USER/boot ]]; then
+    echo "Can find SD card at /run/media/$USER/boot"
+    exit 1
+fi
+
+cp network.txt /run/media/$USER/boot/
+
+PI=/run/media/$USER/rootfs/home/pi
+mkdir -p $PI/.ssh
+chmod 700 $PI/.ssh
+cat ~/.ssh/*.pub >$PI/.ssh/authorized_keys
+chmod 644 $PI/.ssh/*
+chown -R 1000:1000 $PI/.ssh
