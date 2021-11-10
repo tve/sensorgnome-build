249afd97b728a76232af6c451c7c487141091c56
Thorsten von Eicken <tve@voneicken.com>
2021-11-10 13:16:52 -0800

    Make init-sdcard verbose

--- init-sdcard.sh
+++ init-sdcard.sh
@@ -1,9 +1,11 @@
 #! /bin/bash -e
 if [[ ! -d /run/media/$USER/boot ]]; then
     echo "Can find SD card at /run/media/$USER/boot"
+    echo "Did you perhaps forget to remove and re-insert the SD card after flashing?"
     exit 1
 fi
 
+set -x
 cp network.txt /run/media/$USER/boot/
 
 PI=/run/media/$USER/rootfs/home/pi
