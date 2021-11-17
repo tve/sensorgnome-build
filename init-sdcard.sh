3de18eaaefc5489e3a4f8c6d7225302bdd955763
Thorsten von Eicken <tve@voneicken.com>
2021-11-17 15:57:01 -0800

    avoid clobbering pi's ssh keys

--- init-sdcard.sh
+++ init-sdcard.sh
@@ -12,7 +12,7 @@ cp SG_tag_database.sqlite /run/media/$USER/boot/
 PI=/run/media/$USER/rootfs/home/pi
 mkdir -p $PI/.ssh
 chmod 700 $PI/.ssh
-cat ~/.ssh/*.pub >$PI/.ssh/authorized_keys
+cat ~/.ssh/*.pub >>$PI/.ssh/authorized_keys
 chmod 644 $PI/.ssh/*
 chown -R 1000:1000 $PI/.ssh
 sync
