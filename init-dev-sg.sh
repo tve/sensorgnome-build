56ed468ad6e8d80f471346e52e3d2601905cdd76
Thorsten von Eicken <tve@voneicken.com>
2022-10-05 22:13:46 -0700

    add init-dev-sg

---
+++ init-dev-sg.sh
@@ -0,0 +1,40 @@
+#! /bin/bash -e
+# Shell script to fix-up a Sensorgnome to use for development of sensorgnome-control
+
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <hostname>"
+    exit 1
+fi
+
+ssh $1 hostname
+rsync ~/.ssh/tve-git2022 $1:.ssh/
+ssh $1 'git config --global user.email "tve@voneicken.com"; git config --global user.name "Thorsten von Eicken"'
+
+ssh $1 "egrep -q github .ssh/config || tee -a .ssh/config" <<EOF >/dev/null
+Host github.com
+    User git
+    Hostname github.com
+    IdentityFile /home/gnome/.ssh/tve-git2022
+    StrictHostKeyChecking no
+EOF
+
+ssh $1 "cat >sensorgnome-control/run; chmod +x sensorgnome-control/run" <<EOF
+#! /bin/bash
+cd /home/gnome/sensorgnome-control/src
+export NODE_ENV=production
+export NODE_PATH=/home/gnome/sensorgnome-control/src
+export LC_ALL="C.UTF-8"
+node main
+EOF
+
+ssh $1 "sudo -S 'systemctl stop sg-control; systemctl disable sg-control'"
+
+cat <<EOF
+
+# Run the following manually:
+git clone git@github.com:tve/sensorgnome-control.git
+cd sensorgnome-control
+git checkout flexdash
+cd src
+npm install
+EOF
