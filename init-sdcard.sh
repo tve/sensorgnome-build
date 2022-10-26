5c307c7fa1faac07007971b91f776c35b8b33044
Thorsten von Eicken <tve@voneicken.com>
2022-10-26 09:11:35 -0700

    init-sdcard clean-up

--- init-sdcard.sh
+++ init-sdcard.sh
@@ -5,14 +5,11 @@ if [[ ! -d /run/media/$USER/boot ]]; then
     exit 1
 fi
 
-set -x
-#cp network.txt /run/media/$USER/boot/
-#cp SG_tag_database.sqlite /run/media/$USER/boot/
-
 MNT=/run/media/$USER
 PI=$MNT/rootfs/home/gnome
 
 cp ~/.tmux.* $PI || true
 cp ~/.ssh/*.pub $MNT/boot
+#cp SG_tag_database.sqlite /$MNT/boot
 
 sync
