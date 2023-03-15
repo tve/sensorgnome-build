53339a7807deec68f2907b34344d2d59cf9045bb
Thorsten von Eicken <tve@voneicken.com>
2023-03-14 22:30:08 -0700

    init-sdcard: fix bootfs path

--- init-sdcard.sh
+++ init-sdcard.sh
@@ -1,15 +1,17 @@
 #! /bin/bash -e
-if [[ ! -d /run/media/$USER/bootfs ]]; then
-    echo "Cannot find SD card at /run/media/$USER/bootfs"
+MNT=/run/media/$USER
+BOOT=$(echo -n $MNT/boot*)
+
+if [[ ! -d $BOOT ]]; then
+    echo "Cannot find SD card at $MNT/boot*"
     echo "Did you perhaps forget to remove and re-insert the SD card after flashing?"
     exit 1
 fi
 
-MNT=/run/media/$USER
 PI=$MNT/rootfs/home/gnome
 
 cp ~/.tmux.* $PI || true
-cp ~/.ssh/*.pub $MNT/bootfs
+cp ~/.ssh/*.pub $BOOT
 #cp SG_tag_database.sqlite /$MNT/bootfs
 
 sync
