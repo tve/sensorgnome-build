d8f98a18b13ed186aa871d5c26ec82d167693bd0
Thorsten von Eicken <tve@voneicken.com>
2023-03-19 10:04:12 -0700

    Improve CM flash script

--- flash-cm.sh
+++ flash-cm.sh
@@ -1,9 +1,5 @@
 #! /bin/bash -e
 if [[ -z $1 ]]; then echo "usage: $0 <url>"; exit 1; fi
-dev=$(lsblk -d -o NAME,VENDOR,SIZE | egrep 'RPi-MSD.*[2356][0-9]\.[0-9]G' | cut -d" " -f1)
-if [[ -z $dev ]]; then echo "No device found"; exit 1; fi
-dev=/dev/$dev
-echo "Flashing $dev: $(lsblk -dn -o VENDOR,MODEL,SIZE $dev)"
 
 url=$1
 zip=$(basename $1)
@@ -15,6 +11,17 @@ if ! [[ -f $zip ]]; then
 fi
 echo Unmounting
 umount /run/media/$USER/* 2>/dev/null || true
-sudo rpiusbboot
-echo Flashing $dev
+
+dev=$(lsblk -d -o NAME,VENDOR,SIZE | egrep 'RPi-MSD.*[2356][0-9]\.[0-9]G' | cut -d" " -f1)
+if [[ -z $dev ]]; then
+    echo "No device found, running rpiusbboot"
+    sudo rpiusbboot
+    sleep 5
+    dev=$(lsblk -d -o NAME,VENDOR,SIZE | egrep 'RPi-MSD.*[2356][0-9]\.[0-9]G' | cut -d" " -f1)
+    if [[ -z $dev ]]; then echo "No device found."; exit 1; fi
+fi
+dev=/dev/$dev
+
+echo "Flashing $dev: $(lsblk -dn -o VENDOR,MODEL,SIZE $dev)"
+sleep 5
 7z e -so $zip $img | sudo dd of=$dev bs=10M
